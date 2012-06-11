5b1c7bd772a55b88acb6622ca10d7ddd36c1d177
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2012-06-11 19:47:12 +0000

    - adding base esjps for Costumers
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/costumers/trunk@7633 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ src/main/efaps/ESJP/org/efaps/esjp/costumers/Oportunity_Base.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2003 - 2012 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.efaps.esjp.costumers;
+
+import org.efaps.admin.datamodel.Status;
+import org.efaps.admin.event.Parameter;
+import org.efaps.admin.event.Return;
+import org.efaps.admin.program.esjp.EFapsRevision;
+import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.db.Insert;
+import org.efaps.db.Instance;
+import org.efaps.esjp.ci.CICostumers;
+import org.efaps.esjp.ci.CIFormCostumers;
+import org.efaps.esjp.erp.CommonDocument;
+import org.efaps.util.EFapsException;
+
+/**
+ * TODO comment!
+ *
+ * @author The eFaps Team
+ * @version $Id$
+ */
+@EFapsUUID("a64ca997-3c47-4bb2-ab02-06bef4cbe349")
+@EFapsRevision("$Rev$")
+public abstract class Oportunity_Base
+    extends CommonDocument
+{
+
+    /**
+     * Executed from a Command execute vent to create a new CostSheet.
+     *
+     * @param _parameter Parameter as passed from eFaps API.
+     * @return new Return.
+     * @throws EFapsException on error.
+     */
+    public Return create(final Parameter _parameter)
+        throws EFapsException
+    {
+        createDoc(_parameter);
+        return new Return();
+    }
+
+    /**
+     * Create a new CostSheet.
+     *
+     * @param _parameter Parameter as passed from the eFaps API.
+     * @return Instance of the created Document.
+     * @throws EFapsException on error.
+     */
+    protected Instance createDoc(final Parameter _parameter)
+        throws EFapsException
+    {
+        final String date = _parameter.getParameterValue(CIFormCostumers.Costumers_OportunityForm.date.name);
+        final Long contactid = Instance.get(
+                        _parameter.getParameterValue(CIFormCostumers.Costumers_OportunityForm.contact.name)).getId();
+
+        final Insert insert = new Insert(CICostumers.Oportunity);
+        insert.add(CICostumers.Oportunity.Contact, contactid);
+        insert.add(CICostumers.Oportunity.Date, date);
+        insert.add(CICostumers.Oportunity.Salesperson,
+                        _parameter.getParameterValue(CIFormCostumers.Costumers_OportunityForm.salesperson.name));
+        insert.add(CICostumers.Oportunity.Name,
+                        _parameter.getParameterValue(CIFormCostumers.Costumers_OportunityForm.name4create.name));
+        insert.add(CICostumers.Oportunity.Status, Status.find(CICostumers.OportunityStatus.uuid, "Open").getId());
+        insert.execute();
+        return insert.getInstance();
+    }
+
+}
